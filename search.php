e736d8d66ab7e677d0d62addb61e7d8ea69784e8
bpx-network <admin@bpxcoin.cc>
2023-05-02 17:26:43 +0200

    Add search

---
+++ search.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once __DIR__.'/config.inc.php';
+include_once __DIR__.'/inc/main.inc.php';
+
+$pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+$pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$blocks = [];
+
+$page = 0;
+if(isset($_GET['page']) && is_numeric($_GET['page']))
+    $intPage = intval($_GET['page']);
+    if($intPage > 0)
+        $page = $_GET['page'] - 1;
+
+if(!empty($_GET['q'])) {
+    $task = [
+        ':offset' => $page * 50,
+        ':q' => $_GET['q'],
+        ':q2' => '%' + strtolower($_GET['q']) + '%'
+    ];
+
+    $sql = 'SELECT *
+            FROM blocks
+            WHERE height = :q
+            OR LOWER(hash) LIKE :q2
+            ORDER BY height DESC
+            LIMIT 50 OFFSET :offset';
+
+    $q = $pdo -> prepare($sql);
+    $q -> execute($task);
+    $blocks = $q -> fetchAll();
+}
+
+getHeader('Search | BPX Beacon Chain explorer');
+getBlocks('Recent Blocks', $blocks);
+getFooter();
+
+unset($pdo);
+?>
