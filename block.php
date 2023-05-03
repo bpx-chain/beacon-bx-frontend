55b32cac7f353ca153374dafa6170957affaa4d6
bpx-network <admin@bpxcoin.cc>
2023-05-03 10:28:51 +0200

    Single block view

---
+++ block.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once __DIR__.'/config.inc.php';
+include_once __DIR__.'/inc/main.inc.php';
+
+$pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+$pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+if(!isset($_GET['hash']) || strlen($_GET['hash'] != 66)) {
+    header('Location: /404');
+    die();
+}
+
+$task = [
+    ':hash' => $_GET['hash']
+];
+
+$sql = 'SELECT *
+        FROM blocks
+        WHERE hash = :hash';
+
+$q = $pdo -> prepare($sql);
+$q -> execute($task);
+$block = $q -> fetc();
+
+if(!$block) {
+    header('Location: /404');
+    die();
+}
+
+getHeader('Block '.$block['height'].' | BPX Beacon Chain explorer');
+
+echo $block['body'];
+
+getFooter();
+unset($pdo);
+?>
